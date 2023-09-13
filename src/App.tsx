94c46373f5322650c66ca1823cb87a1590e3c4dc
AdamskiJakub <105269736+AdamskiJakub@users.noreply.github.com>
2023-09-13 13:32:07 +0200

    Creating layout for desktop and mobile

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,42 @@
+import { Grid, GridItem, Show } from "@chakra-ui/react";
+
 function App() {
-  return <div>dobra</div>;
+  return (
+    <Grid
+      templateAreas={{
+        base: `"nav" "hero" "footer"`,
+        lg: `"nav nav" "aside hero" "footer footer"`,
+      }}
+      templateColumns={{
+        base: "1fr",
+        lg: "300px 1fr",
+      }}
+      sx={{
+        maxWidth: "1170px",
+        width: "100%",
+        margin: "0 auto",
+      }}
+    >
+      <GridItem area="nav" bg="green" height="120px">
+        Nav
+      </GridItem>
+      <GridItem area="hero" bg="blue" minHeight="100vw" maxHeight="100%">
+        Hero
+      </GridItem>
+      <Show above="lg">
+        <GridItem
+          area="aside"
+          bg="gold"
+          sx={{ display: { base: "none", lg: "block" } }}
+        >
+          Aside
+        </GridItem>
+      </Show>
+      <GridItem area="footer" bg="red" height="120px">
+        Footer
+      </GridItem>
+    </Grid>
+  );
 }
 
 export default App;
