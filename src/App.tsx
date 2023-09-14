0cbb3b14d73c797e3d6fb7374dbfed6d382ff8d9
Rafixx <rafal987.1@o2.pl>
2023-09-14 10:51:19 +0200

    fix: rewrite imports form components folder

--- src/App.tsx
+++ src/App.tsx
@@ -1,55 +1,50 @@
-import { Grid, GridItem, Show } from "@chakra-ui/react";
-import Footer from "./components/footer/Footer";
-
-import Navbar from "./components/Navbar";
-import { Breadcrumb } from "./components/Breadcrumb";
+import { Grid, GridItem, Show } from '@chakra-ui/react';
+import Footer from './components/footer/Footer';
+import Navbar from './components/Navbar';
+import { Hero } from './components/hero/Hero';
 
 function App() {
-	return (
-		<Grid
-			templateAreas={{
-				base: `"nav" "hero" "footer"`,
-				lg: `"nav nav" "aside hero" "footer footer"`,
-			}}
-			templateColumns={{
-				base: "1fr",
-				lg: "300px 1fr",
-			}}
-			sx={{
-				maxWidth: "1170px",
-				width: "100%",
-				margin: "0 auto",
-			}}>
-			<GridItem
-				area='nav'
-				height='120px'
-				display='flex'>
-				<Navbar />
-			</GridItem>
-			<GridItem
-				area='hero'
-				bg='blue'
-				minHeight='100vw'
-				maxHeight='100%'
-				bgColor={"#0A192F"}>
-				<Breadcrumb />
-			</GridItem>
-			<Show above='lg'>
-				<GridItem
-					area='aside'
-					bg='gold'
-					sx={{ display: { base: "none", lg: "block" } }}>
-					Aside
-				</GridItem>
-			</Show>
-			<GridItem
-				area='footer'
-				bg='red'
-				height='120px'>
-				<Footer />
-			</GridItem>
-		</Grid>
-	);
+  return (
+    <Grid
+      templateAreas={{
+        base: `"nav" "hero" "footer"`,
+        lg: `"nav nav" "aside hero" "footer footer"`,
+      }}
+      templateColumns={{
+        base: '100% 1fr',
+        lg: '300px 1fr',
+      }}
+      sx={{
+        maxWidth: '1170px',
+        width: '100%',
+        margin: '0 auto',
+      }}
+    >
+      <GridItem area="nav" height="120px" display="flex">
+        <Navbar />
+      </GridItem>
+      <GridItem
+        area="hero"
+        bg="blue"
+        minHeight="100%"
+        maxHeight="100%"
+        bgColor={'#0A192F'}
+      >
+        <Hero />
+      </GridItem>
+      <Show above="lg">
+        <GridItem
+          area="aside"
+          bg="gold"
+          sx={{ display: { base: 'none', lg: 'block' } }}
+        >
+          Aside
+        </GridItem>
+      </Show>
+      <GridItem area="footer" bg="red" height="120px">
+        <Footer />
+      </GridItem>
+    </Grid>
+  );
 }
-
 export default App;
