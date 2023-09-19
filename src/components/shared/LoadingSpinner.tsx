a2eab86dbf6500f5f5c1e31d442b5676e7339371
Mateusz Pietruczuk <102554956+mpietruczuk07@users.noreply.github.com>
2023-09-19 09:13:16 +0200

    feat: create loading spinner component (#36)

---
+++ src/components/shared/LoadingSpinner.tsx
@@ -0,0 +1,8 @@
+import Spinner from '../../assets/loading-spinner.svg';
+import { Image, keyframes } from '@chakra-ui/react';
+
+const spinAnimation = keyframes({ from: { transform: 'rotate(0deg)' }, to: { transform: 'rotate(360deg)' } });
+export const LoadingSpinner = () => {
+  return (
+    <Image src={Spinner} sx={{ animation: `${spinAnimation} 2s ease infinite` }}></Image>);
+};
