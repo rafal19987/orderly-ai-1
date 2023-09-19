1874ec4dffcaec2837699a940887e28b61d268ff
Mateusz Pietruczuk <102554956+mpietruczuk07@users.noreply.github.com>
2023-09-19 09:16:05 +0200

    feat: create toast notifications (#47)

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,13 @@ import { MainLayout } from './layout/MainLayout';
 import { CategoriesList } from './components/hero/CategoriesList';
 import { ProductsList } from './components/productsList/ProductsList';
 import { ProductCard } from './components/productCard/ProductCard';
+import { Toaster } from 'react-hot-toast';
 import { CategoryForm } from './components/newCategory/CategoryForm';
 
 function App() {
   return (
     <MainLayout>
+      <Toaster/>
       <Routes>
         <Route path='/' element={<CategoriesList />} />
         <Route path='/:categoryName' element={<ProductsList />} />
