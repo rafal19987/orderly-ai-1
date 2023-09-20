8a49408893b785e7e71f7d9ac8978c3c509c2fe8
AnnaIdzkowska <121981221+AnnaIdzkowska@users.noreply.github.com>
2023-09-20 14:23:25 +0200

    feat:Create component input, radio, button

---
+++ src/components/addNewProduct/ProductDetailList.tsx
@@ -0,0 +1,30 @@
+import { Box, Heading } from '@chakra-ui/layout';
+import { Select } from '@chakra-ui/react';
+import { fakeCategoriesAndProducts } from '../../data/fakeCategoriesAndProducts';
+
+
+
+export const ProductDetailList = () => {
+  return (
+    <Box marginRight='10' >
+      <Heading color='text.secondary' fontSize='28px' fontWeight='500' p={3}>
+        Category
+      </Heading>
+      <Select
+        placeholder='Select category'
+        w='100%'
+        h='46px'
+        bg='#1E345B'
+        border='none'
+        fontSize='20px'
+        color='#FFFFFF40'
+        paddingLeft={3}
+      >
+
+        {fakeCategoriesAndProducts.map((category) => (
+          <option key={category.id}>{category.categoryName}</option>
+        ))}
+      </Select>
+    </Box>
+  );
+};
