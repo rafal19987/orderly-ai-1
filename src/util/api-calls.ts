502863c4b06ab96d6c1f0ae9ea1432b2ff063b2f
Mateusz Pietruczuk <102554956+mpietruczuk07@users.noreply.github.com>
2023-09-25 08:19:35 +0200

    Feature/69 add user management to admin panel (#87)
    * feat:Add component new product
    
    * feat:Create component input, radio, button
    
    * Creating basic Layout for Admin Panel
    
    * Fixing basic colors
    
    * adding styles
    
    * fix: create-adminPanel-toggle-function
    
    * Fixing Admin Panel on login
    
    * feat: created api calls for getting all users and updating selected one
    
    * feat: created user list in admin panel
    
    * feat: styled user list and added handler to update their roles
    
    * feat: added handler to remove users
    
    * feat: added additional protection against user deletion by himself
    
    ---------
    
    Co-authored-by: AnnaIdzkowska <121981221+AnnaIdzkowska@users.noreply.github.com>
    Co-authored-by: AdamskiJakub <105269736+AdamskiJakub@users.noreply.github.com>
    Co-authored-by: Rafixx <rafal987.1@o2.pl>

--- src/util/api-calls.ts
+++ src/util/api-calls.ts
@@ -2,17 +2,30 @@ import { TUser } from '@/types/user.ts';
 import { API_URL } from './constants.ts';
 import axios from 'axios';
 
-export const signUp = (user: TUser) =>{
-  return axios.post(API_URL + "users", user);
-}
+export const signUp = (user: TUser) => {
+  return axios.post(API_URL + 'users', user);
+};
 
-export const signIn = (username: string, password: string) =>{
-  return axios.get(API_URL + "users", {
+export const signIn = (username: string, password: string) => {
+  return axios.get(API_URL + 'users', {
     params: {
       username,
       password
     }
   });
-}
+};
 
+export const getAllUsers = () => {
+  return axios.get(API_URL + 'users');
+};
+
+export const updateUser = (userId: number, newRole: string) => {
+  return axios.patch(API_URL + 'users/' + userId, {
+    role: newRole
+  });
+};
+
+export const removeUser = (userId: number) => {
+  return axios.delete(API_URL + 'users/' + userId, {});
+};
 
